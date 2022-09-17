d161e14412f0f092b35eaa12d043e7f59d697f45
gvallabh <84183211+gvallabh@users.noreply.github.com>
2022-09-17 11:16:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.position(150,200);
+}
+
+function draw(){
+}
+
+function Take_Snapshot(){
+    save('myFilterImage.png');
+}
